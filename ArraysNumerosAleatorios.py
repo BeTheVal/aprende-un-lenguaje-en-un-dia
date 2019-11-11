318a344778202d667047533dc46a56b69188a143
Miguel <miguelgutp@gmail.com>
2019-11-11 20:59:43 +0100

    Update ArrNumAl

--- ArraysNumerosAleatorios.py
+++ ArraysNumerosAleatorios.py
@@ -3,3 +3,12 @@ con 20 números enteros aleatorios entre 1 y 100 y que seguidamente los
 muestre por pantalla. A continuación, se deben pasar los números primos
 a las primeras posiciones del array y los no primos a las posiciones restantes. 
 Muestra finalmente el array resultado. """
+
+import random
+
+n = 20
+aleatorios = [random.randint(0, 100) for _ in range(n)]
+print(aleatorios)
+
+
+""" Falta crear otra lista donde los primos de la lista "aleatorios" vayan primero"""
