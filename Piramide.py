d9a07017a9be362c8330b9538998fad74bbe0c8a
Luca <glitchedrom@gmail.com>
2019-11-11 12:52:34 +0100

    piramide resuelto

--- Piramide.py
+++ Piramide.py
@@ -5,12 +5,8 @@ con la altura indicada."""
 print("Este programa dibuja una pirámide")
 alturaintroducida = int(input("Introduzca la altura de la pirámide:"))
 i = 1
-j = 10
 
-planta = 1
-longitudDeLinea = 1
 while i <= alturaintroducida:
-    j = i
     for esp in range(alturaintroducida-i,0,-1):
         print(" ",end='')
     for j in range(i, 0, -1):
